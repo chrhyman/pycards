f5a58a13e2686ceb4e705da7e78032a3cd7dbcd1
Chris Hyman <chris@wugs.me>
2020-10-11 18:56:46 -0700

    Update deck.py

--- deck.py
+++ deck.py
@@ -7,12 +7,12 @@ class Deck(list):
     def __str__(self):
         return ", ".join(str(c) for c in self)
 
-    def deal_n(self, hand, n):
+    def deal_n(self, target_list, n):
         for _ in range(n):
-            hand.append(self.pop())
+            target_list.append(self.pop())
 
-    def deal_card(self, hand, card):
-        hand.append(self.pop_card(card))
+    def deal_card(self, target_list, card):
+        target_list.append(self.pop_card(card))
 
     def pop_card(self, card):
         return self.pop(self.index(card))
@@ -20,7 +20,7 @@ class Deck(list):
     def shuffle(self):
         shuffle(self)
 
-    def top_n(self, n):
+    def reveal_top_n(self, n):
         next_cards = []
         for i in range(n):
             next_cards.append(self[-(i + 1)])
